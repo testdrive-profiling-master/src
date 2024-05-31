e6f6abef5c1ecc445b8d2185b87769be82dd8b57
HyungKi Jeong <clonextop@gmail.com>
2024-05-31 13:46:51 +0900

    fix misc.

--- TestDrive/lua/LuaLibrary.h
+++ TestDrive/lua/LuaLibrary.h
@@ -7,7 +7,7 @@
 #ifndef LUALIBRARY_SOURCE
 
 #if LUABRIDGEDEMO_LUA_VERSION >= 502
-  #include "Lua-5.4.6/src/lua.hpp"
+  #include "lua-5.4.6/src/lua.hpp"
 
 #else
   #error "Unknown LUA_VERSION_NUM"
